879309b4f3658d91b8413b2a2ab003401905784d
Shira Boodaei <shiraboodaei@Shiras-MacBook-Air.local>
2025-01-24 14:58:56 +0200

    Create linkedList tester

---
+++ LinkedListFirstTester.java
@@ -0,0 +1,41 @@
+public class LinkedListFirstTester {
+    public static void main (String[] args){
+        LinkedList list = new LinkedList();
+
+        MemoryBlock block1 = new MemoryBlock(0, 10);
+        MemoryBlock block2 = new MemoryBlock(10, 20);
+        MemoryBlock block3 = new MemoryBlock(30, 30);
+
+        list.addFirst(block1);
+        list.addLast(block3);
+        list.add(1, block2);
+
+        // Expected output: block1, block2, block3
+        System.out.println("Test1 - adding elements: " + list);
+        // Check size 
+        // Expected output:3
+        System.out.println("Current list size: " + list.getSize());
+
+        // Test getNode
+        // Expected output: block2
+        System.out.println("Test2: get second node: " + list.getNode(1).block);
+
+        // Test getBlock 
+        // Expected output: block1
+        System.out.println("Test3: get first block: " + list.getBlock(0));
+
+        // Test indexOf
+        // Expected: 0
+        System.out.println("Test4: Index of block2: " + list.indexOf(block2)); 
+
+        // Test removing elements
+        list.remove(block1);
+        
+        // Expected output: block2, block3
+        System.out.println("Test5: removing block1: " + list);
+        
+        
+
+
+    }
+}
